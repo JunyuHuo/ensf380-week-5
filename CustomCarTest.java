f5f772215c085f186d943d2a89c2693c4e0d6b36
Tyseer Ammar Shahriar <115742900+Bou-Chai@users.noreply.github.com>
2025-02-10 11:57:44 -0700

    Update CustomCarTest.java

--- CustomCarTest.java
+++ CustomCarTest.java
@@ -1,4 +1,4 @@
-public class CarTest {
+public class CustomCarTest {
     public static void main(String[] args) {
         Car car1 = new Car("Hummer H1", new Driver("Joe"), new Engine("v8"));
         Car car2 = car1.copy();
